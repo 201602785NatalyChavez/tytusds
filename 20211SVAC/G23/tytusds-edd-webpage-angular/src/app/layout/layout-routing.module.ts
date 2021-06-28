b7a350ae53ba9f7f3011ac5d2f69a58917589347
201602785NatalyChavez <66294059+201602785NatalyChavez@users.noreply.github.com>
2021-06-28 04:48:37 -0600

    Agregar la tabla a modules

--- 20211SVAC/G23/tytusds-edd-webpage-angular/src/app/layout/layout-routing.module.ts
+++ 20211SVAC/G23/tytusds-edd-webpage-angular/src/app/layout/layout-routing.module.ts
@@ -1,80 +1,82 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import * as path from 'path';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-            { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
-            {
-                path: 'dashboard',
-                loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
-            },
-            {
-                path: 'listas-edd/:idTipoLista',
-                loadChildren: () => import('./estructuras-lineales/listas-edd.module').then((m) => m.ListasEddModule)
-            }
-            ,
-            {
-                path: 'ordenamientos/:idOrdenamiento',
-                loadChildren: () => import('./ordenamientos/ordenamientos.module').then((m) => m.OrdenamientosModule)
-            }
-            ,
-            {
-                path: 'seleccion',
-                loadChildren: () => import('./ordenamientos/seleccion/seleccion.module').then((m) => m.SeleccionModule)
-            }
-            ,
-            {
-                path: 'insercion',
-                loadChildren: () => import('./ordenamientos/insercion/insercion.module').then((m) => m.InsercionModule)
-            }
-            ,
-            {
-                path: 'quick',
-                loadChildren: () => import('./ordenamientos/quick/quick.module').then((m) => m.QuickModule)
-            }
-            ,
-            {
-                path: 'Abb',
-                loadChildren: () => import('./estructuras-arboreas/abb/abb.module').then((m) => m.AbbModule)
-            }
-            ,
-            {
-                path: 'Avl',
-                loadChildren: () => import('./estructuras-arboreas/avl/avl.module').then((m) => m.AvlModule)
-            }
-            ,
-            {
-                path: 'B',
-                loadChildren: () => import('./estructuras-arboreas/b/b.module').then((m) => m.BModule)
-            }
-            ,
-            {
-                path: 'BPlus',
-                loadChildren: () => import('./estructuras-arboreas/bplus/bplus.module').then((m) => m.BplusModule)
-            }
-            ,
-            {
-                path: 'Merkle',
-                loadChildren: () => import('./estructuras-arboreas/merkle/merkle.module').then((m) => m.MerkleModule)
-            }
-            ,
-            {
-                path: 'hash-abierta',
-                loadChildren: () => import('./hash-abierta/hash-abierta.module').then((m) => m.HashAbiertaModule)
-            }
-            ,
-            {
-                path: 'hash-cerrada',
-                loadChildren: () => import('./hash-cerrada/hash-cerrada.module').then((m) => m.HashCerradaModule)
-            }
-        ]
-    }
-];
-
+   
+        {
+            path: '',
+            component: LayoutComponent,
+            children: [
+                { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
+                {
+                    path: 'dashboard',
+                    loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule)
+                },
+                {
+                    path: 'listas-edd/:idTipoLista',
+                    loadChildren: () => import('./estructuras-lineales/listas-edd.module').then((m) => m.ListasEddModule)
+                }
+                ,
+                {
+                    path: 'ordenamientos/:idOrdenamiento',
+                    loadChildren: () => import('./ordenamientos/ordenamientos.module').then((m) => m.OrdenamientosModule)
+                }
+                ,
+                {
+                    path: 'seleccion',
+                    loadChildren: () => import('./ordenamientos/seleccion/seleccion.module').then((m) => m.SeleccionModule)
+                }
+                ,
+                {
+                    path: 'insercion',
+                    loadChildren: () => import('./ordenamientos/insercion/insercion.module').then((m) => m.InsercionModule)
+                }
+                ,
+                {
+                    path: 'quick',
+                    loadChildren: () => import('./ordenamientos/quick/quick.module').then((m) => m.QuickModule)
+                }
+                ,
+                {
+                    path: 'Abb',
+                    loadChildren: () => import('./estructuras-arboreas/abb/abb.module').then((m) => m.AbbModule)
+                }
+                ,
+                {
+                    path: 'Avl',
+                    loadChildren: () => import('./estructuras-arboreas/avl/avl.module').then((m) => m.AvlModule)
+                }
+                ,
+                {
+                    path: 'B',
+                    loadChildren: () => import('./estructuras-arboreas/b/b.module').then((m) => m.BModule)
+                }
+                ,
+                {
+                    path: 'BPlus',
+                    loadChildren: () => import('./estructuras-arboreas/bplus/bplus.module').then((m) => m.BplusModule)
+                }
+                ,
+                {
+                    path: 'Merkle',
+                    loadChildren: () => import('./estructuras-arboreas/merkle/merkle.module').then((m) => m.MerkleModule)
+                }
+                ,
+                {
+                    path: 'hash-cerrada',
+                    loadChildren: () => import('./hash-cerrada/hash-cerrada.module').then((m) => m.HashCerradaModule)
+                }
+                ,
+                {
+                    path: 'hash-abierta',
+                    loadChildren: () => import('./hash-abierta/hash-abierta.module').then((m) => m.HashAbiertaModule)
+                }
+            ]
+        }
+    ];
+    
 @NgModule({
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
